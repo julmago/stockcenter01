edd0d28576e962ebb61179d4fb5705cc0eaaeda5
julmago <47571542+julmago@users.noreply.github.com>
2026-01-30 21:09:41 -0300

    Update _header.php
    cambie lista

--- _header.php
+++ _header.php
@@ -7,7 +7,7 @@ $u = current_user();
   <div style="display:flex;gap:12px;align-items:center;flex-wrap:wrap;">
     <strong>Entrada de Stock</strong>
     <span>|</span>
-    <a href="dashboard.php">Listas</a>
+    <a href="dashboard.php">Listas</a> 
     <a href="list_new.php">Nuevo Listado</a>
     <a href="product_new.php">Nuevo Producto</a>
     <a href="product_list.php">Listado de productos</a>
